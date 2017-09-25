4de31e7cdf9562c3eec4e0ba92a7f912cc7438ff
Fredrich Ombico <fombico@pivotal.io>
2017-09-25 16:49:31 -0400

    Add in-memory db

---
+++ NotesApp.Tests/Services/NoteServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NotesApp.Models;
+using NotesApp.Services;
+using Xunit;
+
+namespace NotesApp.Tests.Services
+{
+    public class NoteServiceTest
+    {
+        private readonly DbContextOptions<NoteContext> _dbContextOptions;
+
+        public NoteServiceTest()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<NoteContext>()
+                .UseInMemoryDatabase()
+                .Options;
+        }
+        
+        [Fact]
+        public void SavesNote()
+        {
+            Note note;
+            using (var context = new NoteContext(_dbContextOptions))
+            {
+                var service = new NoteService(context);
+                note = service.Add(new Note() { Body = "Note"});
+            }
+            Assert.Equal("Note", note.Body);
+        }
+
+        [Fact]
+        public void GetsListOfNotes()
+        {
+            List<Note> notes;
+            using (var context = new NoteContext(_dbContextOptions))
+            {
+                var service = new NoteService(context);
+                service.Add(new Note() { Body = "Note A"});
+                service.Add(new Note() { Body = "Note B"});
+
+                notes = service.GetNotes();
+            }
+            
+            Assert.Equal(3, notes.Count);
+            Assert.Equal("Note 1", notes.ElementAt(0).Body); // assume there's one item already in db
+            Assert.Equal("Note A", notes.ElementAt(1).Body);
+            Assert.Equal("Note B", notes.ElementAt(2).Body);
+        }
+
+        [Fact]
+        public void GetsNoteById()
+        {
+            Note note;
+            using (var context = new NoteContext(_dbContextOptions))
+            {
+                var service = new NoteService(context);
+                var item = service.Add(new Note() { Body = "Note X"});
+
+                note = service.GetById(item.Id);
+            }
+            
+            Assert.Equal("Note X", note.Body);
+        }
+    }
+}
--- NotesApp/Startup.cs
+++ NotesApp/Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NotesApp.Models;
+using NotesApp.Services;
 
 namespace NotesApp
 {
@@ -23,6 +26,10 @@ namespace NotesApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NoteContext>(opt => { opt.UseInMemoryDatabase(); });
+
+            services.AddTransient(typeof(NoteService));
+
             services.AddMvc();
         }
 
