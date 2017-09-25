464b7799c80499852e4b997fad8c44e293ac7154
Fredrich Ombico <fombico@pivotal.io>
2017-09-25 16:49:25 -0400

    Rename controller, add Note model, add api test

---
+++ NotesApp.Tests/Controllers/NotesControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using NotesApp.Models;
+using Xunit;
+
+namespace NotesApp.Tests.Controllers
+{
+    public class NotesControllerTest
+    {
+        
+        private TestServer _server;
+        private HttpClient _client;
+
+        public NotesControllerTest()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+        
+        [Fact]
+        public async void GetAllReturnsList()
+        {
+            var response = await _client.GetAsync("/api/notes");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            
+            var items = JsonConvert.DeserializeObject<Note[]>(responseString);
+            Assert.Equal(1, items.Length);
+            Assert.Equal(1, items[0].Id);
+            Assert.Equal("Note 1", items[0].Body);
+        }
+    }
+}
--- NotesApp/Controllers/NotesController.cs
+++ NotesApp/Controllers/NotesController.cs
@@ -1,41 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using NotesApp.Models;
 
 namespace NotesApp.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class NotesController : Controller
     {
-        // GET api/values
+        // GET api/notes
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Note> Get()
         {
-            return new string[] {"value1", "value2"};
+            return new Note[] { new Note() { Id = 1, Body = "Note 1"} };
         }
 
-        // GET api/values/5
+        // GET api/notes/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/notes
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/notes/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/notes/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
