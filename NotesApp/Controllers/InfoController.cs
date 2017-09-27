c695978fed501cbd6a3d70ab9e8ee655b9b42b87
Fredrich Ombico <fombico@pivotal.io>
2017-09-27 14:28:13 -0400

    Add /api/info endpoint to display cf env variables

---
+++ NotesApp/Controllers/InfoController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class InfoController : Controller
+    {
+        public InfoController(
+            IOptions<CloudFoundryApplicationOptions> appOptions,
+            IOptions<CloudFoundryServicesOptions> serviceOptions)
+        {
+            AppOptions = appOptions.Value;
+            ServiceOptions = serviceOptions.Value;
+        }
+
+        private CloudFoundryServicesOptions ServiceOptions { get; }
+        private CloudFoundryApplicationOptions AppOptions { get; }
+
+        public IActionResult Index()
+        {
+            return Json(new {
+                applicationName = AppOptions.ApplicationName,
+                hostname = ServiceOptions.Services
+                    .SingleOrDefault(s => "notes-db".Equals(s.Name))?
+                    .Credentials["hostname"]
+                    .Value
+            });
+        }
+    }
+}
