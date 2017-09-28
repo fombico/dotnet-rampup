e0ebc98304dcd41d031a8bfb4f0ee87dd672fbf2
Fredrich Ombico <fombico@pivotal.io>
2017-09-28 16:06:20 -0400

    Add steeltoe info actuator

---
+++ NotesApp/Actuators/CustomInfoContributor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Steeltoe.Management.Endpoint.Info;
+
+namespace NotesApp.Actuators
+{
+    public class CustomInfoContributor : IInfoContributor
+    {
+        public void Contribute(IInfoBuilder builder)
+        {
+            Dictionary<string, object> result = new Dictionary<string, object>();
+            result.Add("time", DateTime.Now);
+            builder.WithInfo(result);
+        }
+    }
+}
